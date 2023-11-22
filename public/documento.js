beec1c0ded7d69783ff1a4f1cbad35f45f0aaeeb
andre.matos <andre.matos@sisloc.com.br>
2023-11-22 17:31:14 -0300

    #teste

--- public/documento.js
+++ public/documento.js
@@ -1,5 +1,11 @@
 import { emitirTextoEditor } from "./socket-front-documento.js";
 
+const parametros = new URLSearchParams(window.location.search);
+const nomeDocumento = parametros.get("nome");
+
+let tituloDocumento = document.querySelector("#titulo-documento");
+tituloDocumento.textContent = nomeDocumento || "Documento sem titulo";
+
 let editorTexto = document.querySelector("#editor-texto");
 
 editorTexto.addEventListener("keyup", () => {
